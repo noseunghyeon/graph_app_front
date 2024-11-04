956b1c30bdef9d9b30c1931611bc4b99b0ecfd65
noseunghyeon <ro981009@gmail.com>
2024-11-04 13:30:59 +0900

    11041330

---
+++ src/components/dashboard/Visitors.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeadTitle from "./HeadTitle";
+
+const Visitors = () => {
+  return (
+    <div className="block-wrap">
+      <HeadTitle title="Visitors Insights" />
+    </div>
+  );
+};
+
+export default Visitors;
--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,4 +1,8 @@
+const DOMAIN = "http://lacalhost:8000";
+
 const COUNTRIES_API_URL =
   "https://restcountries.com/v3.1/all?fields=name,flags,languages";
+
+const GET_VISITORS_API_URL = `${DOMAIN}/visitors`;
 
 export { COUNTRIES_API_URL };
